bcf51b884272929a861e7fdc47161d9fb98beb23
PinterKristosz <112863663+PinterKristosz@users.noreply.github.com>
2022-11-09 10:22:15 +0100

    Update Kicsiberobbanabonba.java

--- Kicsiberobbanabonba/src/main/java/com/mycompany/kicsiberobbanabonba/Kicsiberobbanabonba.java
+++ Kicsiberobbanabonba/src/main/java/com/mycompany/kicsiberobbanabonba/Kicsiberobbanabonba.java
@@ -7,24 +7,34 @@ public class Kicsiberobbanabonba {
     int sugar;
     int magassag;
     public void kacsa(){
+	
+	System.out.print("\nAdjon meg egy sugarat! ");
+		Scanner input2 = new Scanner(System.in);
+		int szam3 = input.nextInt() ;
+		System.out.print("Adjon meg még egy magasságot! ");
+		int szam4 = input.nextInt() ;
+		System.out.println("A térfogat: "+(2*pee* pee)*szam1+(2*pee)*szam3*szam4+" cm3");    
+	    
+	    
+	    
         System.out.print("Adj meg egy felszínt: ");
-        Scanner beker = new Scanner(System.in);
-        int sugar = beker.nextInt();
-        System.out.print("Adj meg egy magasságot: ");
-        int magassag = beker.nextInt();
-        System.out.println("A henger felszíne: " + 2 * sugar * magassag + ".");
+       		Scanner beker = new Scanner(System.in);
+       		int sugar = beker.nextInt();
+        	System.out.print("Adj meg egy magasságot: ");
+        	int magassag = beker.nextInt();
+		System.out.println("A henger felszíne: " + 2 * sugar * magassag + ".");
         
         double pee;
-		pee = 3.14;
+	pee = 3.14;
 	
-		System.out.print("Adjon meg egy szamot!: ");
+	System.out.print("Adjon meg egy szamot!: ");
 		Scanner input = new Scanner(System.in);
 		int szam1 = input.nextInt() ;
 		System.out.print("Adjon meg még egy szamot!: ");
 		int szam2 = input.nextInt() ;
 		
-		System.out.println("A 2 szám felszíne: "+pee* pee* szam1 * szam2+"cm2");
-		System.out.println("A 2 szám térfogata: "+(2*pee* pee)*szam1+(2*pee)*szam1*szam2+"cm3");
+	System.out.println("A 2 szám felszíne: "+pee* pee* szam1 * szam2+"cm2");
+	System.out.println("A 2 szám térfogata: "+(2*pee* pee)*szam1+(2*pee)*szam1*szam2+"cm3");
 		
         
         
