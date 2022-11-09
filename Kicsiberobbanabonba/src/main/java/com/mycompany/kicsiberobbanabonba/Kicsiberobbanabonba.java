03e44e5bb9381c3dfef6ddc22a00addc775bd12b
PinterKristosz <112863663+PinterKristosz@users.noreply.github.com>
2022-11-09 10:16:31 +0100

    Update Kicsiberobbanabonba.java

--- Kicsiberobbanabonba/src/main/java/com/mycompany/kicsiberobbanabonba/Kicsiberobbanabonba.java
+++ Kicsiberobbanabonba/src/main/java/com/mycompany/kicsiberobbanabonba/Kicsiberobbanabonba.java
@@ -13,6 +13,23 @@ public class Kicsiberobbanabonba {
         System.out.print("Adj meg egy magasságot: ");
         int magassag = beker.nextInt();
         System.out.println("A henger felszíne: " + 2 * sugar * magassag + ".");
+        
+        double pee;
+		pee = 3.14;
+	
+		System.out.print("Adjon meg egy szamot!: ");
+		Scanner input = new Scanner(System.in);
+		int szam1 = input.nextInt() ;
+		System.out.print("Adjon meg még egy szamot!: ");
+		int szam2 = input.nextInt() ;
+		
+		System.out.println("A 2 szám felszíne: "+pee* pee* szam1 * szam2+"cm2");
+		System.out.println("A 2 szám térfogata: "+(2*pee* pee)*szam1+(2*pee)*szam1*szam2+"cm3");
+		
+        
+        
+        
+        
     }
 } 
 
